c7b84db9e6f972e54dd4edf616eb7c7fcc490825
Brittany Pruneau <69804752+BrittanyPruneau@users.noreply.github.com>
2021-08-21 16:19:10 -0600

    Add files via upload

---
+++ Models/FakeDb.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class FakeDb
+    {
+        // Fields
+
+        private static GuestResponse[] responses = new GuestResponse[10];
+        private static int responseCount = 0;
+
+        // Constructors 
+
+        // Methods 
+        
+        public static void AddResponse(GuestResponse response)
+        {
+            responses[responseCount] = response;
+            responseCount++;
+        }
+        public static int GetResponseCount()
+        {
+            return responseCount; 
+        }
+
+        public static int GetAttendanceCount()
+        {
+            int result = 0; 
+            for (int i=0; i< responses.Length; ++i)
+            {
+                if(responses[i] != null && responses[i].WillAttend == true) 
+                {
+                result++;
+                }
+          }
+            return result; 
+    }
+        public static GuestResponse[] GetResponses()
+        {
+            return responses; 
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse  // POCO Plain Old C# Object 
+    {
+        // Fields
+
+        [Required(ErrorMessage = "Name Is Required")]
+
+        public string    Name              { get; set; } // property create some field behind 
+                                            //the scenes and get a field 
+        [Required(ErrorMessage = "Email Is Required")]
+        [EmailAddress]
+        public string    Email             { get; set; } //property
+
+        [Required(ErrorMessage = "Phone Is Required")]
+        public string    Phone             { get; set; } // Property 
+
+        [Required(ErrorMessage = "WillAttend Is Required")]
+        public bool?     WillAttend        { get; set; } // Property 
+                                                 //   ?  it can have   true  false  NULL 
+        
+        public char      Gender            { get; set; }   
+
+        public string Password             { get; set; }
+
+        // Constructors 
+
+        // methods 
+    }
+}
